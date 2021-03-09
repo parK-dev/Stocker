7ad25484d9ff922d637179e8fa77ac2db0bbe110
Kirk Paradis <kirk.paradis@gmail.com>
2021-03-09 16:16:53 -0500

    Added API for stock tracking and created stock model

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'bootsnap', '>= 1.4.4', require: false
 gem 'bootstrap', '~> 5.0.0.beta2'
 # Models the devise views using bootstrap
 gem 'devise-bootstrap-views', '~> 1.0'
+# API to fetch stock data
+gem 'iex-ruby-client'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_170837) do
+ActiveRecord::Schema.define(version: 2021_03_09_195514) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
